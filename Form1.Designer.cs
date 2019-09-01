8d7b96f423c2480fc6e65529c8be80800196674b
kevin <kevinpalacios90@outlook.com>
2019-09-01 14:44:39 -0500

    se calcula el salario minimo de un empleado

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ejercicio_salario_empleado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btncalcularsalario = new System.Windows.Forms.Button();
+            this.txthorastrabajadas = new System.Windows.Forms.TextBox();
+            this.txtvalorhora = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbltotalsalario = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(567, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "programa para calcular salario de empleados";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(327, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ingrese el valor de cada hora ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(488, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ingrese las horas trabajadas por el empleado";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btncalcularsalario
+            // 
+            this.btncalcularsalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcularsalario.Location = new System.Drawing.Point(467, 217);
+            this.btncalcularsalario.Name = "btncalcularsalario";
+            this.btncalcularsalario.Size = new System.Drawing.Size(110, 90);
+            this.btncalcularsalario.TabIndex = 3;
+            this.btncalcularsalario.Text = "calcular salario";
+            this.btncalcularsalario.UseVisualStyleBackColor = true;
+            // 
+            // txthorastrabajadas
+            // 
+            this.txthorastrabajadas.Location = new System.Drawing.Point(568, 82);
+            this.txthorastrabajadas.Multiline = true;
+            this.txthorastrabajadas.Name = "txthorastrabajadas";
+            this.txthorastrabajadas.Size = new System.Drawing.Size(100, 20);
+            this.txthorastrabajadas.TabIndex = 4;
+            this.txthorastrabajadas.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtvalorhora
+            // 
+            this.txtvalorhora.Location = new System.Drawing.Point(504, 152);
+            this.txtvalorhora.Name = "txtvalorhora";
+            this.txtvalorhora.Size = new System.Drawing.Size(100, 20);
+            this.txtvalorhora.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(60, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "total";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lbltotalsalario
+            // 
+            this.lbltotalsalario.AutoSize = true;
+            this.lbltotalsalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltotalsalario.Location = new System.Drawing.Point(149, 314);
+            this.lbltotalsalario.Name = "lbltotalsalario";
+            this.lbltotalsalario.Size = new System.Drawing.Size(170, 25);
+            this.lbltotalsalario.TabIndex = 7;
+            this.lbltotalsalario.Text = "salario a pagar";
+            this.lbltotalsalario.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbltotalsalario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtvalorhora);
+            this.Controls.Add(this.txthorastrabajadas);
+            this.Controls.Add(this.btncalcularsalario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ingrese las horas trabajadaspor el empleado";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btncalcularsalario;
+        private System.Windows.Forms.TextBox txthorastrabajadas;
+        private System.Windows.Forms.TextBox txtvalorhora;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbltotalsalario;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio_salario_empleado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double horastrabajadas = double.Parse(txthorastrabajadas.Text);
+            double valorhora = double.Parse(txtvalorhora.Text);
+            double total = horastrabajadas = valorhora;
+
+            if (total > 750000)
+            {
+                MessageBox.Show("el empleado gana mas del minimo");
+                lbltotalsalario.Text = total.ToString();
+                lbltotalsalario.Visible = true;
+
+            }
+            else
+            {
+
+
+
+
+            }
+        }
+    }
+}
